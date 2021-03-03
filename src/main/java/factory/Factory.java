9a74c07d600bfb4474bdd0d63377b25a70fb8214
CopyrightAtCLLS <939085525@qq.com>
2021-03-03 09:14:42 +0800

    Factory

--- src/main/java/factory/Factory.java
+++ src/main/java/factory/Factory.java
@@ -5,27 +5,29 @@ public interface Factory {
     Product createProduct();
 
     public static void main(String[] args) {
-        Factory factory=new AppleFactory();
+        Factory factory = new AppleFactory();
         System.out.println(factory.createProduct());
     }
 }
 
-interface Product{
+interface Product {
 
 }
 
-class Apple implements Product{}
+class Apple implements Product {
+}
 
-class Pear implements Product{}
+class Pear implements Product {
+}
 
-class AppleFactory implements Factory{
+class AppleFactory implements Factory {
     @Override
     public Product createProduct() {
         return new Apple();
     }
 }
 
-class PearFactory implements Factory{
+class PearFactory implements Factory {
     @Override
     public Product createProduct() {
         return new Pear();
