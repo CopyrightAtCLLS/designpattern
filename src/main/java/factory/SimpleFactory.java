b25eb0298ced177e11ce4c93e859719a9a3683a2
CopyrightAtCLLS <939085525@qq.com>
2021-03-03 08:52:53 +0800

    Simple Factory

---
+++ src/main/java/factory/SimpleFactory.java
@@ -0,0 +1,28 @@
+package factory;
+
+public class SimpleFactory {
+    public static product createProduct(String type){
+        if("apple".equalsIgnoreCase(type)){
+            return new Apple();
+        }
+        if("pear".equalsIgnoreCase(type)){
+            return new Pear();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(createProduct("Pear"));
+    }
+}
+
+abstract class product{
+
+}
+
+class Apple extends product{
+
+}
+class Pear extends product{
+
+}
