1d74ee500d42b225880efc29ed63b98c05ff126a
CopyrightAtCLLS <939085525@qq.com>
2021-03-03 13:15:06 +0800

    simplest Singleton

--- src/main/java/singleton/basic/Singleton.java
+++ src/main/java/singleton/basic/Singleton.java
@@ -1,14 +1,19 @@
 package singleton.basic;
 
+/**
+ * 1、懒加载
+ * 2、线程不安全
+ */
 public class Singleton {
 
-    private Singleton(){}
+    private Singleton() {
+    }
 
-    private static Singleton instance=null;
+    private static Singleton instance = null;
 
-    public Singleton getInstance(){
-        if(instance==null)
-            instance=new Singleton();
+    public Singleton getInstance() {
+        if (instance == null)
+            instance = new Singleton();
         return instance;
     }
 }
---
+++ src/main/java/singleton/doublecheck/Singleton.java
@@ -0,0 +1,5 @@
+package singleton.doublecheck;
+
+public class Singleton {
+    private static
+}
