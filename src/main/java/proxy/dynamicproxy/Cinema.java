1c25ed389c2737989b357e730b07ccf93b0f4ebe
CopyrightAtCLLS <939085525@qq.com>
2021-03-17 08:58:57 +0800

    Dynamic Proxy

--- src/main/java/proxy/dynamicproxy/Cinema.java
+++ src/main/java/proxy/dynamicproxy/Cinema.java
@@ -4,6 +4,15 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
+/**
+ * 动态代理
+ * 1、接口
+ * 2、实现类
+ * 3、代理类
+ * 优点:
+ * 1、代理类通过invoke方法，可以为任何接口实现类实现相同的增强逻辑，即使并不是同一个接口
+ * 2、代理类动态生成
+ */
 public class Cinema implements InvocationHandler {
     private Object movie;
 
@@ -23,5 +32,13 @@ public class Cinema implements InvocationHandler {
         InvocationHandler cinema = new Cinema(new Avatar());
         Movie avatar = (Movie) Proxy.newProxyInstance(Avatar.class.getClassLoader(), Avatar.class.getInterfaces(), cinema);
         avatar.show();
+
+        System.out.println("----------------------");
+        Movie tenet= (Movie) Proxy.newProxyInstance(Tenet.class.getClassLoader(),Tenet.class.getInterfaces(),new Cinema(new Tenet()));
+        tenet.show();
+
+        System.out.println("----------------------");
+        System.out.println(avatar.getClass().getName()+" "+tenet.getClass().getName());
+        System.out.println(avatar.getClass().hashCode()+" "+tenet.getClass().getName().hashCode());
     }
 }
