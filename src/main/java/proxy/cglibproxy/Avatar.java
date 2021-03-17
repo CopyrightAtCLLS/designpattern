59ec54589992d09292e93dd92ab10a9ccd3aa56e
CopyrightAtCLLS <939085525@qq.com>
2021-03-17 09:09:16 +0800

    to be designated

--- src/main/java/proxy/cglibproxy/Avatar.java
+++ src/main/java/proxy/cglibproxy/Avatar.java
@@ -1,7 +1,6 @@
 package proxy.cglibproxy;
 
-public class Avatar implements Movie {
-    @Override
+public class Avatar {
     public void show() {
         System.out.println("Avatar is being played...");
     }
