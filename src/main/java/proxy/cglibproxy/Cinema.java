516065cc5ef7b1ba642253c68ff4b94d3b7f7cd0
CopyrightAtCLLS <939085525@qq.com>
2021-03-17 09:18:16 +0800

    Cglib Proxy

--- src/main/java/proxy/cglibproxy/Cinema.java
+++ src/main/java/proxy/cglibproxy/Cinema.java
@@ -1,4 +1,24 @@
 package proxy.cglibproxy;
 
-public class Cinema {
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class Cinema implements MethodInterceptor {
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.out.println("Wanda Cinema");
+        methodProxy.invokeSuper(o,objects);
+        System.out.println("End");
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(Avatar.class);
+        enhancer.setCallback(new Cinema());
+        Avatar avatar= (Avatar) enhancer.create();
+        avatar.show();
+    }
 }
