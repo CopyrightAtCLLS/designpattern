a7d181a783463e65f78df44b80e526b95c3fb9b8
CopyrightAtCLLS <939085525@qq.com>
2021-03-31 13:35:40 +0800

    Main

--- src/main/java/builder/Main.java
+++ src/main/java/builder/Main.java
@@ -2,13 +2,18 @@ package builder;
 
 public class Main {
     public static void main(String[] args) {
-        Builder builder=new MacBookBuilder();
-        Director director=new Director(builder);
-        director.construct("Apple Silicon","Intel");
+        Builder builder = new MacBookBuilder();
+        Director director = new Director(builder);
+        director.construct("Apple Silicon", "Intel");
 
         Computer computer = builder.build();
         System.out.println(computer);
-        System.out.println("----------------");
-        System.out.println(new Director(new DellBuilder()).construct(););
+        System.out.println("----------------------------------------------------------");
+
+        builder = new DellBuilder();
+        director = new Director(builder);
+        director.construct("Intel", "Intel");
+        computer = builder.build();
+        System.out.println(computer);
     }
 }
