57d9f04353f8b85510c12e921cc6dc69057bad0c
CopyrightAtCLLS <939085525@qq.com>
2021-03-31 09:40:20 +0800

    Main

--- src/main/java/builder/Main.java
+++ src/main/java/builder/Main.java
@@ -3,5 +3,10 @@ package builder;
 public class Main {
     public static void main(String[] args) {
         Builder builder=new MacBookBuilder();
+        Director director=new Director(builder);
+        director.construct("Apple Silicon","Intel");
+
+        Computer computer = builder.build();
+        System.out.println(computer);
     }
 }
