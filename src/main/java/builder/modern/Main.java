aaf5e8b1144f2a8d2e576d74716b61e3d9942210
CopyrightAtCLLS <939085525@qq.com>
2021-04-11 14:03:16 +0800

    Main

--- src/main/java/builder/modern/Main.java
+++ src/main/java/builder/modern/Main.java
@@ -1,4 +1,8 @@
 package builder.modern;
 
 public class Main {
+    public static void main(String[] args) {
+        Computer.Builder macbook = new Computer.Builder("Apple Silicon", "Intel", "Darwin").setCamera("Embedded");
+        System.out.println(macbook);
+    }
 }
